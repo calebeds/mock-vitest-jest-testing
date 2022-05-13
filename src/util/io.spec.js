6ae608eff26d315ca80c1a3502a99ec1ceb4d09d
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-05-13 20:13:29 -0300

    add mock on io.spec.js file

--- src/util/io.spec.js
+++ src/util/io.spec.js
@@ -1,9 +1,15 @@
-import { it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
 import writeData from './io.js';
+import {promises as fs} from 'fs';
+
+vi.mock('fs');//It will replace all functions inside this module with spy functions | With jest you need to move it before the imports
 
 it('should execute the writeFIle method', () => {
     const testData = 'Test';
     const testFileName = 'test.txt';
+
+    writeData(testData, testFileName);//Because there is fs.writeFile inside of it, it will expect later that it will be called, but with mocks, so it won't execute the third-party code, but only our code
     
-    return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+    // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+    expect(fs.writeFile).toBeCalled();
 });
